22b68c5b1e7027d496cce18de1da69162b39c613
FurkanCetin <furkan@furkancetin.com.tr>
2022-12-18 14:54:21 +0300

    - nlohman Json parser is included - the json file is loaded

--- main.cpp
+++ main.cpp
@@ -1,17 +1,30 @@
 ///////////////////////////////////////////////////////////////
-// MaaS_General Challenge
+// MaaS_General Challenge: Eliminate overlapped convex hulls
 // Written by Furkan Cetin
 // 18/12/2022
 //
 ///////////////////////////////////////////////////////////////
+// JSON Parser library by Neils Lohmann: https://github.com/nlohmann/json
+///////////////////////////////////////////////////////////////
+
 
 //STL Libraries
 #include <iostream>
+#include <fstream>
 
+//JSON Library
+#include "nlohmann/json.hpp"
+
+using json = nlohmann::json;
 using namespace std;
 
 int main()
 {
-    cout << "Hello Earth!!!" << endl;
+    //Loading the json data in where the points data are loaded
+	ifstream strAll("data/convex_hulls (1).json");
+    json dataAll = json::parse(strAll);
+
+    cout << dataAll["convex hulls"] << endl;
+    cout << "There are " << dataAll["convex hulls"].size() << " polygons" << endl;
 
 }
