003be1c20c0c4e65ee0b6ca05ec049e71a8dce4e
FurkanCetin <furkan@furkancetin.com.tr>
2022-12-18 14:44:15 +0300

    initial test code: Hello Earth!

---
+++ main.cpp
@@ -0,0 +1,17 @@
+///////////////////////////////////////////////////////////////
+// MaaS_General Challenge
+// Written by Furkan Cetin
+// 18/12/2022
+//
+///////////////////////////////////////////////////////////////
+
+//STL Libraries
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello Earth!!!" << endl;
+
+}
